4d658662d3baf7f967eefebc0f8532f1db6e0846
DESKTOP-M2491UO\Manh <manhnv.9709@gmail.com>
2021-09-25 17:18:56 +0700

    hoàn hành cau 1

---
+++ manh/cau1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	 int so;
+     printf("nhap so chan \n");
+     scanf("%d",&so);
+     if(so%2==0){
+     	
+     	printf("can bac 2 cua so %d la %lf",so,sqrt(so));
+	 }
+	 else{
+	 	printf("xin vui long nhap lai \n");
+	 }
+}
+
